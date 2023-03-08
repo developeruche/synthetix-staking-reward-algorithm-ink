1fd8d6d5ed19c9861955a30d528f4348fe85f31e
Nwele Uchenna <nweleuchenna2004@gmail.com>
2023-03-08 18:04:00 +0100

    done with token two

--- token_two/lib.rs
+++ token_two/lib.rs
@@ -1,69 +1,40 @@
 #![cfg_attr(not(feature = "std"), no_std)]
+#![feature(min_specialization)]
+        
+#[openbrush::contract]
+pub mod my_psp22 {
+    
+    // imports from openbrush and the psp22 extensions
+	use openbrush::traits::String;
+	use openbrush::traits::Storage;
+	use openbrush::contracts::psp22::extensions::burnable::*;
+	use openbrush::contracts::psp22::extensions::mintable::*;
+	use openbrush::contracts::psp22::extensions::metadata::*;
 
-#[ink::contract]
-mod token_two {
-
-    /// Defines the storage of your contract.
-    /// Add new fields to the below struct in order
-    /// to add new static storage fields to your contract.
     #[ink(storage)]
-    pub struct TokenTwo {
-        /// Stores a single `bool` value on the storage.
-        value: bool,
+    #[derive(Default, Storage)]
+    pub struct Contract {
+    	#[storage_field]
+		psp22: psp22::Data,
+		#[storage_field]
+		metadata: metadata::Data,
     }
-
-    impl TokenTwo {
-        /// Constructor that initializes the `bool` value to the given `init_value`.
+    
+    // Section contains default implementation without any modifications
+	impl PSP22 for Contract {}
+	impl PSP22Burnable for Contract {}
+	impl PSP22Mintable for Contract {}
+	impl PSP22Metadata for Contract {}
+     
+    impl Contract {
         #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
-        }
-
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
-        #[ink(constructor)]
-        pub fn default() -> Self {
-            Self::new(Default::default())
-        }
-
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
-        #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
-        }
-
-        /// Simply returns the current value of our `bool`.
-        #[ink(message)]
-        pub fn get(&self) -> bool {
-            self.value
-        }
-    }
-
-    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
-    /// module and test functions are marked with a `#[test]` attribute.
-    /// The below code is technically just normal Rust code.
-    #[cfg(test)]
-    mod tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
-
-        /// We test if the default constructor does its job.
-        #[ink::test]
-        fn default_works() {
-            let token_two = TokenTwo::default();
-            assert_eq!(token_two.get(), false);
-        }
-
-        /// We test a simple use case of our contract.
-        #[ink::test]
-        fn it_works() {
-            let mut token_two = TokenTwo::new(false);
-            assert_eq!(token_two.get(), false);
-            token_two.flip();
-            assert_eq!(token_two.get(), true);
+        pub fn new(initial_supply: Balance, name: Option<String>, symbol: Option<String>, decimal: u8) -> Self {
+            let mut _instance = Self::default();
+			_instance._mint_to(_instance.env().caller(), initial_supply).expect("Should mint"); 
+			_instance.metadata.name = name;
+			_instance.metadata.symbol = symbol;
+			_instance.metadata.decimals = decimal;
+			_instance
         }
     }
 }
